f65a7ce042e85614cbc406a6bcf814e10f9d1715
RobertoCaro <78422684+RobertoCaro@users.noreply.github.com>
2025-03-12 15:14:22 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+<!-- service-worker.js -->
+const CACHE_NAME = 'math-pwa-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
